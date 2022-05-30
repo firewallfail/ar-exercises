5c517948a52a1203e469f5563490c60b66b9b694
firewallfail <firewall.fail@hotmail.com>
2022-05-30 15:18:55 -0400

    Exercise 6 fixed to work for 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -16,4 +16,4 @@ puts "----------"
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 @store2.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store2.employees.create(first_name: "Hall", last_name: "Brent", hourly_rate: 22)
+@store2.employees.create(first_name: "Hall", last_name: "Brent", hourly_rate: 40)
